1580fd52561bcfe99ff82f670d9b9f3ffef3e767
Junell777 <janejunell777@gmail.com>
2022-09-07 10:06:34 +0300

    time in next one second

---
+++ js1/next_sec.js
@@ -0,0 +1,7 @@
+// Write - Your - Code
+const hours = Number(prompt("Enter hours:"));
+const mins = Number(prompt("Enter minutes:"));
+const seconds = Number(prompt("Enter seconds:"));
+const next_sec = seconds+1;
+
+alert(`In a second, the time will be ${hours}:${mins}:${next_sec}`);
