7a03be9a8517848a5a097f295e4d4ca305a80288
Junell777 <janejunell777@gmail.com>
2022-09-07 09:51:41 +0300

    days in months

--- js1/month_days.js
+++ js1/month_days.js
@@ -1,9 +1,9 @@
 const month = Number(prompt("Enter month number to see the number of days:"));
 
-if(month === 1 || month === 3 || month === 5 || month === 7 || month === 9 || month === 11){
+if(month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12){
     console.log(`Month ${month} has 31 days!`);
 }
-else if(month === 4 || month ===6 || month === 8 || month === 10 || month === 12){
+else if(month === 4 || month ===6 || month === 9 || month === 11){
     console.log(`Month ${month} has 30 days!`);
 }
 else if(month === 2){
