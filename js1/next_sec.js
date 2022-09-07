f757c90841b629d446a4c69f4ad45cc07ba0bc2c
Junell777 <janejunell777@gmail.com>
2022-09-07 19:12:09 +0300

    next second

--- js1/next_sec.js
+++ js1/next_sec.js
@@ -1,7 +1,51 @@
-// Write - Your - Code
-const hours = Number(prompt("Enter hours:"));
-const mins = Number(prompt("Enter minutes:"));
-const seconds = Number(prompt("Enter seconds:"));
-const next_sec = seconds+1;
+//enter time: h, m, s
 
-alert(`In a second, the time will be ${hours}:${mins}:${next_sec}`);
+let hours = Number(prompt("Enter the hours:"));
+let minutes = Number(prompt("Enter minutes:"));
+let seconds = Number(prompt("Enter seconds:"));
+
+//if 0=<h<=23; 0<=m<=59; 0<=s<=59
+
+if( 0<= hours && hours <= 23 && 0 <= minutes && minutes <= 59 && 0 <= seconds && seconds <= 59){ 
+
+//s++
+
+	seconds++;
+
+
+//    if s==60;s=0
+
+	if(seconds == 60){
+        seconds = 0;
+
+//        m++;
+        
+
+		minutes++;   
+//    if m==60;m=0
+
+
+		if(minutes == 60){
+			minutes = 0;
+//        h++
+
+
+			hours++;
+//    if h==24;h=0
+
+			if(hours == 24){
+
+				hours = 0;
+//        s++
+
+				seconds++;
+    }
+    }
+    }
+
+	console.log(`The time in one next second will be ${hours}:${minutes}:${seconds}`);
+
+}else {
+
+	alert("Invalid input!");
+}
